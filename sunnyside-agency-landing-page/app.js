8c12176d3ffd163f6f1b307529c446cec773830e
Marvin Kamau <stallionofoz@gmail.com>
2021-10-19 17:45:16 +0300

    Styling  and navBar functionality added

---
+++ sunnyside-agency-landing-page/app.js
@@ -0,0 +1,8 @@
+// add navBar functionality
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    $(".toggle-btn").click(()=>{
+        $("#nav-bar").slideToggle(700);
+    })
+  });
